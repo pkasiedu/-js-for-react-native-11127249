d9ffa0e7b7f5ce54a6cab527bb50f95bfdb51bca
pkasiedu <pkasiedu007@st.ug.edu.gh>
2024-05-19 05:00:05 +0000

    Add files via upload

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -12,3 +12,22 @@ function processArray(arr) {
 const inputArray = [1, 2, 3, 4, 5];
 const resultArray = processArray(inputArray);
 console.log(resultArray); 
+function processArray(arr) {
+    return arr.map(num => {
+        if (num % 2 === 0) {
+            return num * num;
+        } else {
+            return num * 3;
+        }
+    });
+}
+function formatArrayStrings(strArr, numArr) {
+    return strArr.map((str, index) => {
+        const num = numArr[index];
+        if (num % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
